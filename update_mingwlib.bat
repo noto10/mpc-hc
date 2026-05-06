fef25df5da943eb495464155df35d53061e5848e
noto10 <noto10@users.noreply.github.com>
2026-05-07 05:07:12 +0800

    Enhance update_mingwlib.bat for GCC compatibility
    Updated script to improve compatibility with newer GCC paths and added error handling for missing libraries.

--- update_mingwlib.bat
+++ update_mingwlib.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 REM (C) 2009-2019 see Authors.txt
-REM Fixed for MSYS2 MinGW-w64 compatibility
+REM Fixed for MSYS2 MinGW-w64 compatibility (Newer GCC paths)
 
 SETLOCAL
 SET "FILE_DIR=%~dp0"
@@ -9,39 +9,47 @@ PUSHD "%FILE_DIR%"
 IF EXIST "build.user.bat" CALL "build.user.bat"
 
 IF NOT DEFINED MPCHC_MINGW64 (
-    ECHO ERROR: Please define MPCHC_MINGW64 environment variable
+    ECHO ERROR: MPCHC_MINGW64 is not defined.
     ENDLOCAL
     EXIT /B 1
 )
-
-IF NOT EXIST "%MPCHC_MINGW64%\bin\gcc.exe" (
-    ECHO ERROR: gcc.exe not found in %MPCHC_MINGW64%\bin
-    ENDLOCAL
-    EXIT /B 1
-)
-
-REM Get GCC Version
-FOR /f "tokens=1 delims=." %%K IN ('"%MPCHC_MINGW64%\bin\gcc.exe" -dumpversion') DO SET "GCCVER=%%K"
 
 SET LIBDIR=lib
 MKDIR "%LIBDIR%" 2>NUL
 MKDIR "%LIBDIR%64" 2>NUL
 
-REM Copy libgcc.a
-COPY /V /Y "%MPCHC_MINGW64%\lib\gcc\x86_64-w64-mingw32\%GCCVER%\libgcc.a" "%LIBDIR%64\" >NUL
+ECHO Copying MinGW libraries...
 
-REM Copy and Strip libmingwex.a (64-bit)
-REM Modern MSYS2 puts this in x86_64-w64-mingw32\lib
-COPY /V /Y "%MPCHC_MINGW64%\x86_64-w64-mingw32\lib\libmingwex.a" "%LIBDIR%64\" >NUL
+REM 1. Copy libgcc.a
+REM Try standard MSYS2 location first, then fallback
+IF EXIST "%MPCHC_MINGW64%\lib\libgcc.a" (
+    COPY /V /Y "%MPCHC_MINGW64%\lib\libgcc.a" "%LIBDIR%\" >NUL
+    COPY /V /Y "%MPCHC_MINGW64%\lib\libgcc.a" "%LIBDIR%64\" >NUL
+) ELSE (
+    ECHO WARNING: Could not find libgcc.a
+)
 
-REM Try to remove unused objects. Ignore errors as object names change between GCC versions.
-lib -remove:lib64_libmingwex_a-strtoimax.o -remove:lib64_libmingwex_a-strtof.o -remove:lib64_libmingwex_a-cos.o -remove:lib64_libmingwex_a-sin.o -remove:lib64_libmingwex_a-pow.o -remove:lib64_libmingwex_a-sqrt.o -remove:lib64_libmingwex_a-sqrtf.o -remove:lib64_libmingwex_a-powi.o "%LIBDIR%64\libmingwex.a" || REM
+REM 2. Copy and Strip libmingwex.a (64-bit)
+REM Newer MSYS2 puts this in \lib, older puts it in \x86_64-w64-mingw32\lib
+SET "SRC_LIB="
+IF EXIST "%MPCHC_MINGW64%\lib\libmingwex.a" SET "SRC_LIB=%MPCHC_MINGW64%\lib\libmingwex.a"
+IF EXIST "%MPCHC_MINGW64%\x86_64-w64-mingw32\lib\libmingwex.a" SET "SRC_LIB=%MPCHC_MINGW64%\x86_64-w64-mingw32\lib\libmingwex.a"
 
-REM Rename the result to .lib temporarily
-RENAME "%LIBDIR%64\libmingwex.a" "libmingwex.lib" 2>NUL
-
-REM Move the stripped library to the final name expected by the linker
-MOVE /Y "%LIBDIR%64\libmingwex.lib" "%LIBDIR%64\libmingwex.a" >NUL
+IF DEFINED SRC_LIB (
+    COPY /V /Y "%SRC_LIB%" "%LIBDIR%64\libmingwex.a" >NUL
+    
+    REM Strip unused objects (Ignore errors if object names differ)
+    lib -remove:lib64_libmingwex_a-strtoimax.o -remove:lib64_libmingwex_a-strtof.o -remove:lib64_libmingwex_a-cos.o -remove:lib64_libmingwex_a-sin.o -remove:lib64_libmingwex_a-pow.o -remove:lib64_libmingwex_a-sqrt.o -remove:lib64_libmingwex_a-sqrtf.o -remove:lib64_libmingwex_a-powi.o "%LIBDIR%64\libmingwex.a" || REM
+    
+    REM Rename to .lib temporarily, then back to .a to satisfy MSBuild
+    RENAME "%LIBDIR%64\libmingwex.a" "libmingwex.lib" 2>NUL
+    MOVE /Y "%LIBDIR%64\libmingwex.lib" "%LIBDIR%64\libmingwex.a" >NUL
+) ELSE (
+    ECHO ERROR: Could not find libmingwex.a in expected locations.
+    ECHO Checked:
+    ECHO 1. %MPCHC_MINGW64%\lib\libmingwex.a
+    ECHO 2. %MPCHC_MINGW64%\x86_64-w64-mingw32\lib\libmingwex.a
+)
 
 ECHO MinGW libraries updated successfully.
 ENDLOCAL
