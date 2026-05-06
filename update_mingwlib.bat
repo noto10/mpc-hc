69e91b2bbbbf217db9f988c4f4a6789cf65b3ef4
noto10 <noto10@users.noreply.github.com>
2026-05-07 04:54:27 +0800

    Refactor update_mingwlib.bat for MSYS2 compatibility
    Removed outdated comments and streamlined error handling for environment variable checks. Updated library copying and stripping processes for MinGW compatibility.

--- update_mingwlib.bat
+++ update_mingwlib.bat
@@ -1,20 +1,6 @@
 @ECHO OFF
 REM (C) 2009-2019 see Authors.txt
-REM
-REM This file is part of MPC-HC.
-REM
-REM MPC-HC is free software; you can redistribute it and/or modify
-REM it under the terms of the GNU General Public License as published by
-REM the Free Software Foundation; either version 3 of the License, or
-REM (at your option) any later version.
-REM
-REM MPC-HC is distributed in the hope that it will be useful,
-REM but WITHOUT ANY WARRANTY; without even the implied warranty of
-REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-REM GNU General Public License for more details.
-REM
-REM You should have received a copy of the GNU General Public License
-REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+REM Fixed for MSYS2 MinGW-w64 compatibility
 
 SETLOCAL
 SET "FILE_DIR=%~dp0"
@@ -22,35 +8,41 @@ PUSHD "%FILE_DIR%"
 
 IF EXIST "build.user.bat" CALL "build.user.bat"
 
-IF DEFINED MPCHC_MINGW64 GOTO VarOk
-ECHO ERROR: Please define MPCHC_MINGW64 environment variable
-ENDLOCAL
-EXIT /B
-
-:VarOk
-
-FOR /f "tokens=1,2 delims=" %%K IN (
-  '%MPCHC_MINGW64%\bin\gcc -dumpversion'
-) DO (
-  SET "gccver=%%K" & Call :SubGCCVer %%gccver:*Z=%%
+IF NOT DEFINED MPCHC_MINGW64 (
+    ECHO ERROR: Please define MPCHC_MINGW64 environment variable
+    ENDLOCAL
+    EXIT /B 1
 )
 
+IF NOT EXIST "%MPCHC_MINGW64%\bin\gcc.exe" (
+    ECHO ERROR: gcc.exe not found in %MPCHC_MINGW64%\bin
+    ENDLOCAL
+    EXIT /B 1
+)
+
+REM Get GCC Version
+FOR /f "tokens=1 delims=." %%K IN ('"%MPCHC_MINGW64%\bin\gcc.exe" -dumpversion') DO SET "GCCVER=%%K"
+
 SET LIBDIR=lib
+MKDIR "%LIBDIR%" 2>NUL
+MKDIR "%LIBDIR%64" 2>NUL
 
-COPY /V /Y "%MPCHC_MINGW64%\lib\gcc\i686-w64-mingw32\%gccver%\libgcc.a" %LIBDIR%\
-COPY /V /Y "%MPCHC_MINGW64%\lib\gcc\x86_64-w64-mingw32\%gccver%\libgcc.a" %LIBDIR%64\
+REM Copy libgcc.a
+COPY /V /Y "%MPCHC_MINGW64%\lib\gcc\x86_64-w64-mingw32\%GCCVER%\libgcc.a" "%LIBDIR%64\" >NUL
 
-COPY /V /Y "%MPCHC_MINGW64%\i686-w64-mingw32\lib\libmingwex.a" %LIBDIR%\
-lib -remove:lib32_libmingwex_a-strtoimax.o -remove:lib32_libmingwex_a-strtof.o -remove:lib32_libmingwex_a-cos.o -remove:lib32_libmingwex_a-sin.o -remove:lib32_libmingwex_a-pow.o -remove:lib32_libmingwex_a-sqrt.o -remove:lib32_libmingwex_a-powi.o "%LIBDIR%\libmingwex.a" 
-MOVE /Y %LIBDIR%\libmingwex.lib %LIBDIR%\libmingwex-stripped.a
+REM Copy and Strip libmingwex.a (64-bit)
+REM Modern MSYS2 puts this in x86_64-w64-mingw32\lib
+COPY /V /Y "%MPCHC_MINGW64%\x86_64-w64-mingw32\lib\libmingwex.a" "%LIBDIR%64\" >NUL
 
-COPY /V /Y "%MPCHC_MINGW64%\x86_64-w64-mingw32\lib\libmingwex.a" %LIBDIR%64\
-lib -remove:lib64_libmingwex_a-strtoimax.o -remove:lib64_libmingwex_a-strtof.o -remove:lib64_libmingwex_a-cos.o -remove:lib64_libmingwex_a-sin.o -remove:lib64_libmingwex_a-pow.o -remove:lib64_libmingwex_a-sqrt.o -remove:lib64_libmingwex_a-sqrtf.o -remove:lib64_libmingwex_a-powi.o "%LIBDIR%64\libmingwex.a"
-MOVE /Y %LIBDIR%64\libmingwex.lib %LIBDIR%64\libmingwex-stripped.a
+REM Try to remove unused objects. Ignore errors as object names change between GCC versions.
+lib -remove:lib64_libmingwex_a-strtoimax.o -remove:lib64_libmingwex_a-strtof.o -remove:lib64_libmingwex_a-cos.o -remove:lib64_libmingwex_a-sin.o -remove:lib64_libmingwex_a-pow.o -remove:lib64_libmingwex_a-sqrt.o -remove:lib64_libmingwex_a-sqrtf.o -remove:lib64_libmingwex_a-powi.o "%LIBDIR%64\libmingwex.a" || REM
 
-EXIT /B
+REM Rename the result to .lib temporarily
+RENAME "%LIBDIR%64\libmingwex.a" "libmingwex.lib" 2>NUL
 
-:SubGCCVer
-SET gccver=%*
-@ECHO gccver=%*
-EXIT /B
+REM Move the stripped library to the final name expected by the linker
+MOVE /Y "%LIBDIR%64\libmingwex.lib" "%LIBDIR%64\libmingwex.a" >NUL
+
+ECHO MinGW libraries updated successfully.
+ENDLOCAL
+EXIT /B 0
